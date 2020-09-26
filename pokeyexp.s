13f5c71613fc37838e85801ac80f04af009b69f7
Ivo van Poorten <ivopvp@gmail.com>
2020-09-26 19:39:51 +0200

    disable non functional UI elements

--- pokeyexp.s
+++ pokeyexp.s
@@ -378,10 +378,12 @@ loc_clock_channel3_line = *+1
     dta $00
 loc_two_tone_line = *+1
     dta $42, a(two_tone_off_line)
+.if 0
     dta $30
     dta $42, a(sweep_line)
     dta $10
     dta $02, $02, $02, $02, $02, $02, $02, $02
+.fi
 
     dta $41, a(dl)
 
@@ -506,6 +508,7 @@ down_keys_line
 
 ; ---------------------------------------------------------------------------
 
+.if 0
 sweep_line
     dta d'         Press ', d' START '*, d' to sweep         '
     dta d' CTRL-', d'R'*, d' Resolution   : 16 bit           '
@@ -516,7 +519,7 @@ sweep_line
     dta d' CTRL-', d'P'*, d' Play time    : 1s               '
     dta d' CTRL-', d'G'*, d' Gap time     : 0.1s             '
     dta d' CTRL-', d'X'*, d' Poly Reset   : once             '
-
+.fi
 
 ; ---------------------------------------------------------------------------
 
