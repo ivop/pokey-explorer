a4ac82777497507cc1446e4320253d83872ff8d3
Ivo van Poorten <ivopvp@gmail.com>
2020-10-07 18:14:29 +0200

    set var_tuning_key_was_pressed before check for stereo, otherwise in mono mode, it will constantly decrease this variable for no reason :)

--- pokeyexp.s
+++ pokeyexp.s
@@ -1224,13 +1224,13 @@ no_polyreset
     case_inc_16bit_key 'W'-64, var_sweep_end_value
     case_dec_16bit_key 'E'-64, var_sweep_end_value
 
+    lda #1
+    sta var_tuning_key_was_pressed
+
     lda stereo_pokey
     beq no_tuning_keys
 
     ; Tuning keys
-    lda #1
-    sta var_tuning_key_was_pressed
-
     lda keybuf
     case_var_ctrl_key 'T', var_tuning_enabled, 1
 
