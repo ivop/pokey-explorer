0b95a9594e068209b7edb77ed4b2c74743d52d96
Ivo van Poorten <ivopvp@gmail.com>
2020-10-08 02:31:44 +0200

    print all values and document the formulas

--- util/genfreqtab.c
+++ util/genfreqtab.c
@@ -1,5 +1,16 @@
+// ---------------------------------------------------------------------------
+// Generate tuning tables -- by Ivo van Poorten (C)2020 - 0BSD
+// output RFC4180 CSV table with header
+// ---------------------------------------------------------------------------
+
+#define TUNE_TO_A (440.0)   // floating point double in Hertz
+
+// ---------------------------------------------------------------------------
+
 #include "stdio.h"
 #include "math.h"
+
+// ---------------------------------------------------------------------------
 
 #define NUMBER_OF_OCTAVES 9
 
@@ -21,9 +32,11 @@
 #define MAIN_CLOCK_PAL  1773447.0
 #define MAIN_CLOCK_NTSC 1789790.0
 
+// Well-tempered tuning
+//
 // Fn = F0 * a^n
 
-#define F0 (440.0) // Hz
+#define F0 (TUNE_TO_A) // Hz
 #define a_100cents (pow(2.0,(1.0/12.0)))
 
 #define FORMULA(n) ((F0) * (pow(a_100cents,(n))))
@@ -42,15 +55,24 @@
 double equal_tempered_frequencies[NUMBER_OF_OCTAVES*12];
 
 int main(int argc, char **argv) {
-    int i;
+    int i, v_pal, v_ntsc;
+    double e, f_pal, f_ntsc;
 
-    printf("n, equal_tempered, nearest_PAL, nearest_NTSC, "
-           "PAL hex, NTSC hex\n");
+    printf("n, equal_tempered_frequency, PAL_hex, NTSC_hex, "
+           "PAL_frequency, NTSC_frequency\n");
+
     for (i=0; i<(NUMBER_OF_OCTAVES*12); i++) {
-        equal_tempered_frequencies[i] = FORMULA(i+C1);
-        printf("%i, %0.2f\n", i, equal_tempered_frequencies[i]);
-    }
+        e = equal_tempered_frequencies[i] = FORMULA(i+C1);
 
-//    printf("test 440Hz: %f\n", FREQ_TO_VALUE(MAIN_CLOCK_PAL, 440));
-//    printf("reverse test: %f\n", VALUE_TO_FREQ(MAIN_CLOCK_PAL, 2008));
+        v_pal  = round(FREQ_TO_VALUE(MAIN_CLOCK_PAL,  e));
+        v_ntsc = round(FREQ_TO_VALUE(MAIN_CLOCK_NTSC, e));
+
+        f_pal  = VALUE_TO_FREQ(MAIN_CLOCK_PAL,  v_pal );
+        f_ntsc = VALUE_TO_FREQ(MAIN_CLOCK_NTSC, v_ntsc);
+
+        printf("%i, %0.2f, %04x, %04x, %0.2f, %0.2f\n",
+                i, equal_tempered_frequencies[i],
+                v_pal, v_ntsc,
+                f_pal, f_ntsc);
+    }
 }
