35da5f8596fc5a094d21f9143aa58874df169fd0
Ivo van Poorten <ivopvp@gmail.com>
2020-10-23 19:07:32 +0200

    add scancode defines

---
+++ scancodes.s
@@ -0,0 +1,77 @@
+
+; Atari 8-bit keyboard scancodes
+
+    scancode_esc       = 28
+    scancode_1         = 31
+    scancode_2         = 30
+    scancode_3         = 26
+    scancode_4         = 24
+    scancode_5         = 29
+    scancode_6         = 27
+    scancode_7         = 51
+    scancode_8         = 53
+    scancode_9         = 48
+    scancode_0         = 50
+    scancode_lt        = 54
+    scancode_gt        = 55
+    scancode_del       = 52
+
+    scancode_tab       = 44
+    scancode_q         = 47
+    scancode_w         = 46
+    scancode_e         = 42
+    scancode_r         = 40
+    scancode_t         = 45
+    scancode_y         = 43
+    scancode_u         = 11
+    scancode_i         = 13
+    scancode_o         = 8
+    scancode_p         = 10
+    scancode_minus     = 14
+    scancode_equal     = 15
+    scancode_return    = 12
+
+    scancode_a         = 63
+    scancode_s         = 62
+    scancode_d         = 58
+    scancode_f         = 56
+    scancode_g         = 61
+    scancode_h         = 57
+    scancode_j         = 1
+    scancode_k         = 5
+    scancode_l         = 0
+    scancode_semicolon = 2
+    scancode_plus      = 6
+    scancode_asterisk  = 7
+    scancode_caps      = 60
+
+    scancode_z         = 23
+    scancode_x         = 22
+    scancode_c         = 18
+    scancode_v         = 16
+    scancode_b         = 21
+    scancode_n         = 35
+    scancode_m         = 37
+    scancode_comma     = 32
+    scancode_period    = 34
+    scancode_slash     = 38
+    scancode_inverse   = 39
+
+    scancode_space     = 33
+    scancode_f1        = 3
+    scancode_f2        = 4
+    scancode_f3        = 19
+    scancode_f4        = 20
+    scancode_help      = 17
+
+; scan code modifiers
+; these work for all keys
+
+    scancode_shift     = +64 
+    scancode_control   = +128
+
+; control+shift
+; non-working keys are: JKL;+* ZXCVB F1 F2 F3 F4 and HELP
+
+    scancode_control_shift = +192
+
