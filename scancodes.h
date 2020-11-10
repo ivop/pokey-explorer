61b47f183931d9caec4f26b815739aba69c1f6af
Ivo van Poorten <ivopvp@gmail.com>
2020-11-11 00:56:22 +0100

    add C version of scancodes. note that both are not used by pokey-explorer

---
+++ scancodes.h
@@ -0,0 +1,77 @@
+
+// Atari 8-bit keyboard scancodes
+
+#define SCANCODE_esc       28
+#define SCANCODE_1         31
+#define SCANCODE_2         30
+#define SCANCODE_3         26
+#define SCANCODE_4         24
+#define SCANCODE_5         29
+#define SCANCODE_6         27
+#define SCANCODE_7         51
+#define SCANCODE_8         53
+#define SCANCODE_9         48
+#define SCANCODE_0         50
+#define SCANCODE_lt        54
+#define SCANCODE_gt        55
+#define SCANCODE_del       52
+
+#define SCANCODE_tab       44
+#define SCANCODE_q         47
+#define SCANCODE_w         46
+#define SCANCODE_e         42
+#define SCANCODE_r         40
+#define SCANCODE_t         45
+#define SCANCODE_y         43
+#define SCANCODE_u         11
+#define SCANCODE_i         13
+#define SCANCODE_o         8
+#define SCANCODE_p         10
+#define SCANCODE_minus     14
+#define SCANCODE_equal     15
+#define SCANCODE_return    12
+
+#define SCANCODE_a         63
+#define SCANCODE_s         62
+#define SCANCODE_d         58
+#define SCANCODE_f         56
+#define SCANCODE_g         61
+#define SCANCODE_h         57
+#define SCANCODE_j         1
+#define SCANCODE_k         5
+#define SCANCODE_l         0
+#define SCANCODE_semicolon 2
+#define SCANCODE_plus      6
+#define SCANCODE_asterisk  7
+#define SCANCODE_caps      60
+
+#define SCANCODE_z         23
+#define SCANCODE_x         22
+#define SCANCODE_c         18
+#define SCANCODE_v         16
+#define SCANCODE_b         21
+#define SCANCODE_n         35
+#define SCANCODE_m         37
+#define SCANCODE_comma     32
+#define SCANCODE_period    34
+#define SCANCODE_slash     38
+#define SCANCODE_inverse   39
+
+#define SCANCODE_space     33
+#define SCANCODE_f1        3
+#define SCANCODE_f2        4
+#define SCANCODE_f3        19
+#define SCANCODE_f4        20
+#define SCANCODE_help      17
+
+// scan code modifiers           e.g. scancode_x + scancode shift
+// these work for all keys
+
+#define SCANCODE_shift     64
+#define SCANCODE_control   128
+
+// control+shift
+// non-working keys are: jkl semicolon plus asterisk zxcvb f1 f2 f3 f4 help
+
+#define SCANCODE_control_shift 192
+
